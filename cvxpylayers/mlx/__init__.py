ffcc88517ec7042f54018cd825660ee2d2a6e834
Narasimhan <20cs01075@iitbbs.ac.in>
2025-11-21 02:25:54 +0530

    feat: add basic mlx package file

---
+++ cvxpylayers/mlx/__init__.py
@@ -0,0 +1,9 @@
+"""
+CVXPy Layer for Apple MLX Framework
+
+This package provides a differentiable convex optimization
+layer that integrates
+CVXPY with MLX's automatic differentiation system.
+"""
+
+from .cvxpylayer import CvxpyLayer, make_cvxpy_layer   # noqa: F401
